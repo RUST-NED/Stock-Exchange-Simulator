f59213b499bd2ad71927d2da762a732541a3ae9a
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2022-01-06 10:50:35 +0500

    Added initial project

---
+++ Project/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+          }
+    });
+
+    win.loadFile('index.html');
+}
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    createWindow()
+
+    // Open a window if none are open (macOS)​
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    })
+})
+
+
+// Quit the app when all windows are closed (Windows & Linux)​
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
